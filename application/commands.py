1c3438b1f04aeaaeb44e11346796d4549bc5c9ed
Iván Portillo <ivanportillo@me.com>
2018-10-27 13:03:48 +0200

    Working

--- application/commands.py
+++ application/commands.py
@@ -3,12 +3,15 @@ from application import bot
 from boto3.session import Session
 import requests
 import os
+import uuid
 
 TOKEN = os.environ.get('BOT_TOKEN')
+AUDD_API_TOKEN = os.environ.get('AUDD_API_TOKEN')
 
 session = Session(aws_access_key_id=os.environ.get('AWS_S3_ACCESS_KEY'),
                   aws_secret_access_key=os.environ.get('AWS_S3_SECRET_KEY'))
 s3 = session.resource('s3')
+s3Client = session.client('s3')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -27,6 +30,27 @@ def echo_message(message):
     path = getPathRequest['result']['file_path']
     rawFileRequest = requests.get('https://api.telegram.org/file/bot'+ TOKEN + '/' + path)
 
+    fileKey = str(uuid.uuid4())
     
-    s3.Bucket('musictelegram').put_object(Key='file.oga', Body=rawFileRequest.content)
-    bot.reply_to(message, "HOLA")
+    upload = s3.Bucket('musictelegram').put_object(Key=fileKey, Body=rawFileRequest.content, ACL='public-read')
+    
+    bucket_location = s3Client.get_bucket_location(Bucket='musictelegram')
+    object_url = "http://s3-{0}.amazonaws.com/{1}/{2}".format(
+    bucket_location['LocationConstraint'],
+    'musictelegram',
+    fileKey)
+
+    getSongInfo = requests.get('https://api.audd.io/?api_token=' + AUDD_API_TOKEN + '&url=' + object_url).json()
+
+    songInfo = getSongInfo['result']
+    songTitle = songInfo['title']
+    songArtist = songInfo['artist']
+    songAlbum = songInfo['album']
+
+    messageToSend = """
+    *Título: * {}
+    *Artista: * {}
+    *Album: * {}
+        """.format(songTitle, songArtist, songAlbum)
+
+    bot.reply_to(message, messageToSend, parse_mode="Markdown")
